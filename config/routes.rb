144f21d87e35cb1d6556559b7796d0ed40308bcd
theFest <stjepanb83@gmail.com>
2022-04-28 23:40:28 +0200

    Controller home index
    generate controller, modify routes, added about page(to home controller), boostrap template(modify application.html.erb), header and footer partials, navbar added, css and JS frameworks added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
